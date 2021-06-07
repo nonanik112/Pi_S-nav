1da20723a99650f1a636ee3dfe084c0b8622a7b0
nonanik112 <METEHAN_1306@hotmail.com>
2021-06-08 02:20:14 +0300

    Emlak Api Sınav

---
+++ Emlak APi/Emlak Api/EmlakApp/EmlakApp.DataAccess/EmlakContext.cs
@@ -0,0 +1,17 @@
+﻿using EmlakApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmlakApp.DataAccess
+{
+    public class EmlakContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("");
+        }
+
+        public DbSet<Ev> Evler { get; set; }
+    }
+}
